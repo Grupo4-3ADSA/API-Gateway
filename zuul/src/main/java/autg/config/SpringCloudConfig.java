d0b7ea536e2971a042134e8901d20f5edf8ef81b
jonas.junior <jonas.junior@valemobi.com.br>
2022-04-21 20:34:03 -0300

    subindo api gateway

---
+++ zuul/src/main/java/autg/config/SpringCloudConfig.java
@@ -0,0 +1,19 @@
+package autg.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringCloudConfig {
+
+	@Bean
+	public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(r -> r.path("/companies/**")
+						.uri("lb://register"))
+				.build();
+				
+	}
+}
